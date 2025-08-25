58543c9b0343079ae801a6505a4244999f7d2800
Guhirwa <christianguhirwa74@gmail.com>
2025-08-25 12:23:20 +0200

    Updating Form - action attribute to handle for me some functionalities like grabbing input data, prevent full form reloading and reset the form after submit

--- src/assets/components/InputField.jsx
+++ src/assets/components/InputField.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
 const InputField = () => {
+
+    const[ingredient, setIngredient] = React.useState(["Beef", "Meat"])
+
+    function handleInputData(formData) {
+        const newIngredient = formData.get("ingredient")
+        if(!newIngredient) return
+        setIngredient(prevIngredient => [...prevIngredient, newIngredient])
+    } 
+
+    const listOfIngredients = ingredient.map(ingre => <li key={ingre}>{ingre}</li>)
+
   return (
-    <form className='mt-14 flex justify-center gap-6'>
+    <>
+        <form action={handleInputData} className='mt-14 flex justify-center gap-6'>
         <label htmlFor="inputField"></label>
         <input placeholder='e.g: beef' name='ingredient' className='p-2 w-80 border border-gray-400 rounded-sm' id='inputField' type="text" />
         <button className='rounded-sm p-2 text-white bg-black w-42'>+ Add Ingredient</button>
     </form>
+
+    <ul className='flex flex-col text-center mt-10 list-disc list-inside'>{listOfIngredients}</ul>
+    </>
   )
 }
 
