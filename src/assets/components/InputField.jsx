668b1124af3e247b9798dc29fb31b7f62e40c95c
Guhirwa <christianguhirwa74@gmail.com>
2025-08-27 15:08:22 +0200

    Refining The form to Add/Remove - By using the setState method I managed to add or remove an ingredient - Throught mapping each list element have its own delete button

--- src/assets/components/InputField.jsx
+++ src/assets/components/InputField.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import deleteBtn from '../images/deleteBtn.svg'
 
 const InputField = () => {
 
-    const[ingredient, setIngredient] = React.useState(["Beef", "Meat"])
+    const[ingredient, setIngredient] = React.useState([])
 
     function handleInputData(formData) {
         const newIngredient = formData.get("ingredient")
@@ -11,20 +12,28 @@ const InputField = () => {
             return
         }
         setIngredient(prevIngredient => [...prevIngredient, newIngredient])
-    } 
+    }
 
-    const listOfIngredients = ingredient.map(ingre => <li key={ingre}>{ingre}</li>)
+    const listOfIngredients = ingredient.map((ingre, index) => (
+       <div className='flex gap-4 mb-3 -ml-64' key={index}>
+        <li>{ingre}</li>
+        <button onClick={() => {
+          setIngredient(prev => prev.filter((element, elementIndex) => elementIndex !== index))
+        }} 
+          className='cursor-pointer'><img className='bg-black rounded-full p-1 h-auto w-6' src={deleteBtn} alt="Delete Button" /></button>
+      </div>
+    ))
 
   return (
-    <>
-        <form action={handleInputData} className='mt-14 flex justify-center gap-6'>
+    <div className='flex flex-col items-center'>
+        <form action={handleInputData} className='mt-6'>
         <label htmlFor="inputField"></label>
         <input placeholder='e.g: beef' name='ingredient' className='p-2 w-80 border border-gray-400 rounded-sm' id='inputField' type="text" />
-        <button className='rounded-sm p-2 text-white bg-black w-42'>+ Add Ingredient</button>
+        <button className='ml-8 cursor-pointer rounded-sm p-2 text-white bg-black w-42'>+ Add Ingredient</button>
     </form>
 
-    <ul className='w-fit mx-auto mt-10 list-disc list-inside'>{listOfIngredients}</ul>
-    </>
+    <ul className='mt-10 list-disc list-inside'>{listOfIngredients}</ul>
+    </div>
   )
 }
 
